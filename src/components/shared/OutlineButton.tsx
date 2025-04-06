ac915fbdf3040bacfbc2cd3caefbec24a4e82c54
i-mml <mohamadja114@gmail.com>
2025-04-06 16:52:33 +0330

    update outline button transition

--- src/components/shared/OutlineButton.tsx
+++ src/components/shared/OutlineButton.tsx
@@ -12,7 +12,8 @@ const OutlineButton = ({
     <button
       style={{ height: `${height}px`, ...style }}
       className={`
-         text-center max-h-[50px] box-border font-semibold rounded-xl no-underline text-sm shadow-none bg-transparent border border-b-[5px] border-primary text-primary
+         text-center max-h-[50px] box-border font-semibold rounded-xl no-underline text-sm shadow-none bg-transparent border border-b-[5px] border-primary text-primary         !transition-all
+        !duration-500 
         ${
           buttonProps.disabled
             ? "!border-backgroundDisabled !text-backgroundDisabled border-b-disabled"
