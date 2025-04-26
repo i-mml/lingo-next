5a691de98b8175aba41dd7e516c72970082cddd1
i-mml <mohamadja114@gmail.com>
2025-04-26 23:23:03 +0330

    add toast styles

--- src/components/providers/ToastContainer.tsx
+++ src/components/providers/ToastContainer.tsx
@@ -1,6 +1,28 @@
 import React from "react";
-import { ToastContainer } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { CheckCircle, AlertCircle, XCircle, Info } from "lucide-react";
+
+// Create custom icons for toast notifications
+export const toastIcons = {
+  success: <CheckCircle size={20} />,
+  error: <XCircle size={20} />,
+  warning: <AlertCircle size={20} />,
+  info: <Info size={20} />,
+};
+
+// Custom toast functions with consistent styling
+export const showToast = {
+  success: (message: string) =>
+    toast.success(message, { icon: toastIcons.success }),
+
+  error: (message: string) => toast.error(message, { icon: toastIcons.error }),
+
+  warning: (message: string) =>
+    toast.warning(message, { icon: toastIcons.warning }),
+
+  info: (message: string) => toast.info(message, { icon: toastIcons.info }),
+};
 
 const ToastContainerWithConfig = () => {
   return (
@@ -8,11 +30,13 @@ const ToastContainerWithConfig = () => {
       position="top-right"
       autoClose={3000}
       hideProgressBar
-      newestOnTop={false}
+      newestOnTop
+      closeOnClick
       rtl
       pauseOnFocusLoss
       draggable
       pauseOnHover
+      theme="colored"
     />
   );
 };
