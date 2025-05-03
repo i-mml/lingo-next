226066c4a29aa736ff097e092bd15fbeb71e8e9b
i-mml <mohamadja114@gmail.com>
2025-05-03 14:35:00 +0330

    make the units page as a component

---
+++ src/views/app/units/types.ts
@@ -0,0 +1,17 @@
+export type Unit = {
+  id: number;
+  unit_id: string;
+  title: string;
+  level_id: number;
+  level_text: string;
+  grammar_description: string;
+  practical_use: string;
+  summary: string;
+  finished: boolean;
+  words: number;
+  accents: string[];
+  images: string[];
+  covers: string[];
+  activities: any[];
+  is_locked?: boolean;
+};
