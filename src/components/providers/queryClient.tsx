6550ce43683e1cfc39b68d156cb0d8ae3691d022
i-mml <mohamadja114@gmail.com>
2025-05-18 10:09:49 +0330

    add some padding to bottom nav

--- src/components/providers/queryClient.tsx
+++ src/components/providers/queryClient.tsx
@@ -3,7 +3,6 @@
 
 import { ReactNode, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 interface Props {
   children: ReactNode;
@@ -31,9 +30,6 @@ export default function QueryProvider({ children }: Props) {
   );
 
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
--- src/components/shared/BottomNavigation.tsx
+++ src/components/shared/BottomNavigation.tsx
@@ -67,7 +67,7 @@ const BottomNavigation = () => {
       animate={{ y: 0 }}
       className="fixed bottom-0 left-0 right-0 bg-backgroundMain border-t border-borderMain z-50"
     >
-      <div className="max-w-screen-xl mx-auto px-4">
+      <div className="max-w-screen-xl mx-auto px-4 pb-2">
         <div className="flex justify-around items-center h-16">
           {navItems.map((item) => {
             const isActive = pathname === item.href;
